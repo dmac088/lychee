08237c0bc6708ab95a04c214f28858fb5426107d
Dan <danielmackie82@gmail.com>
2022-07-12 10:46:00 +0800

    added renew.sh which should be added to crontab

---
+++ renew.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+data_path="data/certbot"
+
+docker rm --force certbot
+
+docker run -it -d  \
+    --entrypoint="/bin/sh" \
+    --rm \
+    --name certbot \
+    -v $(pwd)/$data_path/conf:/etc/letsencrypt \
+    -v $(pwd)/$data_path/www:/var/www/certbot \
+    --network my-net \
+    certbot/certbot
+
+docker exec certbot sh -c 'certbot renew --webroot -w /var/www/certbot --agree-tos'
+    
+echo "Reloading nginx..."
+docker exec my-react-container nginx -s reload
+echo "Done!"
+echo "Certificate is already up-to-date."
+
+docker container stop certbot
