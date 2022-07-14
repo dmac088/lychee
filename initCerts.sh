42d2fcb3108debd7ec84c96062d422db10dad887
Dan <danielmackie82@gmail.com>
2022-07-14 10:22:27 +0800

    moved application to subdomain app.littlebagshop.com

--- initCerts.sh
+++ initCerts.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-domains=(littlebagshop.com)
+domains=(app.littlebagshop.com)
 rsa_key_size=4096
 data_path="data/certbot"
 email="danielmackie82@gmail.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
 
-if [ -d "$data_path" ]; then
+echo $data_path/$domains
+
+if [ -d "$data_path/conf/live/$domains" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
     exit
