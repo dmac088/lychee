5d1424ad14fc7b78723317a6bab78051a08a5610
Dan <danielmackie82@gmail.com>
2022-07-18 15:01:50 +0800

    added taiga.sv2.io

--- runDocker.sh
+++ runDocker.sh
@@ -2,11 +2,11 @@
 
 data_path="data/certbot"
 
-docker run  --rm -it -d \
+docker run  --rm -it -d  \
             --name my-react-container  \
             -p 8070:8070 \
-            -p 80:8080 \
-            -p 443:4443 \
+            -p 8002:8080 \
+            -p 4443:4443 \
             -v $(pwd)/$data_path/conf:/etc/letsencrypt \
             -v $(pwd)/$data_path/www:/var/www/certbot \
             -v $(pwd)/data/nginx:/etc/nginx/conf.d \
