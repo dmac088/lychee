0c6334cff2c5d8a62d1e6fd34462eac3c580db37
Daniel Mackie <danielmackie@daniels-mbp.lan>
2022-08-23 14:01:02 +0800

    mproved numbers on the checkout

--- src/components/Layout/Checkout/Checkout.js
+++ src/components/Layout/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { CardGroup } from 'react-bootstrap';
 import { useSelector } from 'react-redux';
+import { Spinner } from '../../Layout/Helpers/animation';
 
 function Checkout() {
 
@@ -8,45 +10,50 @@ function Checkout() {
 
   const renderItems = (items) => {
     return items.map((i, index) => {
+      console.log(i)
       return <li key={index}>{i.data.itemDesc} x {i.data.itemQty}<span>${i.data.bagItemTotal}</span></li>
     })
   }
-    return(
-      <React.Fragment>
-        <div className="page-section section mb-50">
-          <div className="container">
-            <div className="row">
-              <div className="col-12">
-                <form action="#" className="checkout-form">
-                  
-                        <div className="col-12 mb-60">
 
-                          <h4 className="checkout-title">Cart Total</h4>
+  return (
+    (bag.loading) 
+    ? <Spinner />
+    :
+    <React.Fragment>
+      <div className="page-section section mb-50">
+        <div className="container">
+          <div className="row">
+            <div className="col-12">
+              <form action="#" className="checkout-form">
 
-                          <div className="checkout-cart-total">
+                <div className="col-12 mb-60">
 
-                            <h4>Product <span>Total</span></h4>
+                  <h4 className="checkout-title">Cart Total</h4>
 
-                            <ul>
-                              {renderItems(bagContents.items)}
-                            </ul> 
+                  <div className="checkout-cart-total">
 
-                            <p>Sub Total <span>${bag.totalAmount}</span></p>
-                            <p>Shipping Fee <span>$00.00</span></p>
+                    <h4>Product <span>Total</span></h4>
 
-                            <h4>Grand Total <span>${bag.totalAmount}</span></h4>
+                    <ul>
+                      {renderItems(bagContents.items)}
+                    </ul>
 
-                          </div>
+                    <p>Sub Total <span>${bag.data.subTotalAmount}</span></p>
+                    <p>Shipping Fee <span>${bag.data.shippingItem.markdownPrice}</span></p>
 
-                        </div>
-                </form>
+                    <h4>Grand Total <span>${bag.data.grandTotalAmount}</span></h4>
 
-              </div>
+                  </div>
+
+                </div>
+              </form>
+
             </div>
           </div>
         </div>
-      </React.Fragment>
-    )
-  }
+      </div>
+    </React.Fragment>
+  )
+}
 
-  export default Checkout;
+export default Checkout;
